8be5e65b2142da090892c54d87f2c6824c2bc059
Magnús Benediktsson <bened@csc.kth.se>
2015-05-05 13:19:30 +0200

    visualize

---
+++ viewdistributed3d.m
@@ -0,0 +1,13 @@
+load 0data.txt
+
+a = reshape(X0data(1,:),[5 5 5]);
+ind = find(a);
+[x,y,z] = ind2sub(size(a),ind);
+l=[x,y,z];
+figure();
+hold on;
+for i=1:4
+    voxel(l(i,:),[1 1 1],'r',0.7);
+end
+axis equal;
+axis([0 10 0 10 0 10]);
