93a84b99e7d8ae1a1c6231d18b047538f5ec03c5
dario-wat <dario.vidas92@gmail.com>
2015-05-05 15:02:07 +0200

    Changed representation of output files

--- visualize3d.m
+++ visualize3d.m
@@ -1,35 +1,23 @@
-data = load('testvis');
-w = 5;
-h = 5;
-d = 5;
-it = 30;
+%data = load('testvis');
+file = fopen('testvis', 'r');
+header = textscan(file, '%d %d %d %d\n', 1);
+it = header{1};
+w = header{2};
+h = header{3};
+d = header{4};
+data = textscan(file, '%s', iter, 'Delimiter', '\n');
+data = data{1};
 
 for j = 1:it
-    offset = (j-1) * h * d;
-    data3d = zeros([w, h, d]);
-    for i = 1:d
-        ids = offset + (i-1)*h+1 : offset + i*h;
-        data3d(:,:,i) = data(ids,:);
-    end
-
-    ids = find(data3d);
-    N = size(ids, 1);
-    xs = zeros(N, 1);
-    ys = zeros(N, 1);
-    zs = zeros(N, 1);
+    row = str2num(data{j});
+    l = length(row);
+    p = l/3;
+    m = reshape(row, [3, p]);
+    
     clf;
-    for i = 1:N
-        id = ids(i);
-        zs(i) = floor(id / (w*h));
-        id = mod(id, w*h);
-        ys(i) = floor(id / w);
-        xs(i) = mod(id, w);
-        voxel([xs(i), ys(i), zs(i)], [1,1,1], 'r', 0.7);
+    for i = 1:p
+        voxel(m(:,i), [1,1,1], 'r', 0.7);
     end
-
-    %scatter3(xs, ys, zs, 225*ones([N, 1]), 's', 'filled');
     axis([0 w 0 h 0 d]);
-    %axis equal
     pause(0.3);
-    %figure;
 end
