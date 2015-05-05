84f6c7b9bc0f7d578b9f0b88ff9f4e24878213f2
dario-wat <dario.vidas92@gmail.com>
2015-05-05 16:21:34 +0200

    Changing visualization to support more files

---
+++ opt.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	int w = 90, h = 90, d = 90, R = 8;
+	int p, q, r;
+
+	for (p = 1; p < R; p++) {
+		for (q = 1; q < R; q++) {
+			for (r = 1; r < R; r++) {
+				
+			}
+		}
+	}
+}
--- visualize3d.m
+++ visualize3d.m
@@ -1,13 +1,16 @@
-%data = load('testvis');
-file = fopen('testvis', 'r');
-header = textscan(file, '%d %d %d %d\n', 1);
-it = header{1};
-w = header{2};
-h = header{3};
-d = header{4};
-data = textscan(file, '%s', iter, 'Delimiter', '\n');
-data = data{1};
+[R, it, w, h, d] = load('metainfodata.txt');
 
+data = cell(R, it);
+for i = 1:R
+    file = fopen([num2str(i-1) 'data.txt'], 'r');
+    f_data = textscan(file, '%s', iter, 'Delimiter', '\n');
+    f_data = f_data{1};
+    for j = 1:it
+        data{i,j} = str2num(f_data{j});
+    end
+end
+
+%{
 for j = 1:it
     row = str2num(data{j});
     l = length(row);
@@ -21,3 +24,4 @@ for j = 1:it
     axis([0 w 0 h 0 d]);
     pause(0.3);
 end
+%}
